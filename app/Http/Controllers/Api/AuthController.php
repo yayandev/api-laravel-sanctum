22480fea5a723c5a71c7560ad795af381a8d9ca9
yayanfr20 <yayanfathurohman20@gmail.com>
2023-12-13 00:40:32 +0700

    add auth

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+
+class AuthController extends Controller
+{
+    public function Register(Request $request) {
+        $validator  = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'confirm_password' => 'required|same:password',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Terjadi Kesalahan!',
+                'data' => $validator->errors()
+            ]);
+        }
+
+        $input = $request->all();
+
+        $input['password'] = bcrypt($input['password']);
+
+        $user = User::create($input);
+
+        $success['email'] =  $user->email;
+        $success['name'] =  $user->name;
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Registrasi Berhasil!',
+            'data' => $success
+        ]);
+    }
+
+    public function Login(Request $request) {
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $auth =Auth::user();
+            $success['token'] =  $auth->createToken('auth_token')->plainTextToken;
+            $success['name'] =  $auth->name;
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Login Berhasil!',
+                'data' => $success
+            ]);
+        }else{
+            return response()->json([
+                'status' => false,
+                'message' => 'Email atau Password Anda Salah!',
+                'data' => null
+            ]);
+        }
+    }
+
+    public function Logout(Request $request ) {
+
+        $user = $request->user();
+
+        $user->tokens()->delete();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Logout Berhasil!',
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/profile', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'Logout']);
+
+Route::post('/register', [AuthController::class, 'Register']);
+Route::post('/login', [AuthController::class, 'Login']);
